951d375a45ec81f52da945e1ca5325c3c6173d64
Crafty12345 <hunterdylan482@gmail.com>
2023-03-10 22:44:06 +0800

    Added win detection.

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ import random
 import argparse
 import time
 
+
+pygame.init()
 log_id = time.time()
 
 # Board size: 7 cols, 6 rows (7x6)
@@ -12,8 +14,10 @@ log_id = time.time()
 pointer_yellow_img = pygame.image.load("yellow_triangle.png")
 pointer_red_img = pygame.image.load("red_triangle.png")
 
+end_game_font = pygame.font.Font("freesansbold.ttf",56)
+
 parser = argparse.ArgumentParser()
-parser.add_argument("mode",action="store",type=str)
+parser.add_argument("mode", action="store", type=str)
 args = parser.parse_args()
 print(args.mode)
 
@@ -48,7 +52,7 @@ class Circle:
 
 
 class Pointer:
-    def __init__(self, size, pos,status) -> None:
+    def __init__(self, size, pos, status) -> None:
         self.size = size*2
         self.pos = pos
         self.selected = 0
@@ -102,26 +106,59 @@ def check_slot_availability(grid, req_col):
     col_sum = 0
     for i in range(len(grid)):
         col_sum += grid[i][req_col].status
-        #TODO: Move if is_col_full to out of for loop
+        # TODO: Move if is_col_full to out of for loop
         if is_col_full(grid, req_col):
             return -1
     if col_sum == 0:
-        #print("Slot empty.")
+        # print("Slot empty.")
         return (req_col, 5)
     for i in reversed(range(len(grid))):
         if grid[i][req_col].status == 0:
             return (req_col, i)
+        
+def check_for_win(grid, statuses) -> int:
+    for status in statuses:
+        for n in range(len(grid)):
+            for i in range(len(grid[n])):
+                #Check for horizontal win
+                if i+3 <= len(grid[n])-1:
+                    if (grid[n][i].status == status and
+                        grid[n][i+1].status == status and
+                        grid[n][i+2].status == status and
+                        grid[n][i+3].status == status):
+                            return status
+                if n+3 <= len(grid)-1:
+                    #Check for vertical win
+                    if (grid[n][i].status == status and
+                        grid[n+1][i].status == status and
+                        grid[n+2][i].status == status and
+                        grid[n+3][i].status == status):
+                            return status
+                if i+3 <= len(grid[n])-1 and n+3 <= len(grid)-1:
+                    #Check for down-right diagonal win
+                    if (grid[n][i].status == status and
+                        grid[n+1][i+1].status == status and
+                        grid[n+2][i+2].status == status and
+                        grid[n+3][i+3].status == status):
+                            return status
+                if i+3 <= len(grid[n])-1 and n-3 >= 0:
+                    #Check for up-right diagonal win
+                        if (grid[n][i].status == status and
+                        grid[n-1][i+1].status == status and
+                        grid[n-2][i+2].status == status and
+                        grid[n-3][i+3].status == status):
+                            return status
+    return 0
 
 
-pygame.init()
+
+pygame.display.set_caption("Connect Four Bot")
 
 SCREEN_SIZE = (600, 600)
 BOARD_SIZE = pygame.Rect(0, 100, SCREEN_SIZE[0], SCREEN_SIZE[1])
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
-
-circle = Circle(screen, Point(200, 200), 75, 0)
 board_padding = 7
 slots = [[], [], [], [], [], []]
 rand_row = random.randint(0, 5)
@@ -134,42 +171,47 @@ for n in range(0, 6):
             board_padding*2), n*((BOARD_SIZE.height-board_padding)/7)+110+(slot_size)), slot_size, 0)
         slots[n].append(circle)
 slots[5][rand_col].update_status(2)
-available_cols = list(range(0,7))
+available_cols = list(range(0, 7))
 
 
-pointer = Pointer(slot_size, Point(board_padding*2, 50),1)
+pointer = Pointer(slot_size, Point(board_padding*2, 50), 1)
+win_status = 0
 
-print(get_grid_status(slots))
+game_ended = False
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and pointer.selected != 0:
-                pointer.update_position(pointer.selected-1)
-            if event.key == pygame.K_RIGHT and pointer.selected != 6:
-                pointer.update_position(pointer.selected+1)
-            if event.key == pygame.K_RETURN:
-                available_slot = check_slot_availability(
-                    slots, pointer.selected)
-                if available_slot != -1:
-                    slots[available_slot[1]][available_slot[0]].update_status(pointer.status)
-                elif available_slot == -1:
-                    pass
-                if args.mode == "multiplayer":
-                    pointer.status = 2 if pointer.status+1==2 else 1
-                elif args.mode == "random":
-                    #red
-                    rand_col = random.randint(0,6)
+            if not game_ended:
+                if event.key == pygame.K_LEFT and pointer.selected != 0:
+                    pointer.update_position(pointer.selected-1)
+                if event.key == pygame.K_RIGHT and pointer.selected != 6:
+                    pointer.update_position(pointer.selected+1)
+                if event.key == pygame.K_RETURN:
                     available_slot = check_slot_availability(
-                                    slots,rand_col)
-                    while available_slot == -1:
-                        rand_col = random.randint(0,6)
-                        available_slot = check_slot_availability(
-                                        slots,rand_col)
-                slots[available_slot[1]][available_slot[0]].update_status(2)
-                with open(f"logs/log_{log_id}.txt","a+") as f:
-                    f.write(get_grid_status(slots) + "\n")
+                        slots, pointer.selected)
+                    if available_slot != -1:
+                        slots[available_slot[1]][available_slot[0]
+                                                ].update_status(pointer.status)
+                    elif available_slot == -1:
+                        pass
+                    if args.mode == "multiplayer":
+                        pointer.status = 2 if pointer.status+1 == 2 else 1
+                    # elif args.mode == "random":
+                    #     # red
+                    #     rand_col = random.randint(0, 6)
+                    #     available_slot = check_slot_availability(
+                    #         slots, rand_col)
+                    #     while available_slot == -1:
+                    #         rand_col = random.randint(0, 6)
+                    #         available_slot = check_slot_availability(
+                    #             slots, rand_col)
+                    # slots[available_slot[1]][available_slot[0]].update_status(2)
+                    
+                    win_status = check_for_win(slots,[1,2])
+                    with open(f"logs/log_{log_id}.txt", "a+") as f:
+                        f.write(get_grid_status(slots) + "\n")
 
         if event.type == pygame.QUIT:
             running = False
@@ -182,6 +224,17 @@ while running:
             pygame.draw.circle(circle.surface, circle.colour,
                                (circle.pos.x, circle.pos.y), circle.radius)
     pointer.draw()
+    if win_status != 0:
+        game_ended = True
+        if win_status == 2:
+            text = end_game_font.render("Yellow Wins!",True,pygame.Color(222,205,58),None)
+            #print("Yellow wins!")
+        elif win_status == 1:
+            text = end_game_font.render("Red Wins!",True,pygame.Color(235, 64, 52),None)
+            #print("Red wins!")
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_SIZE[0]/2,SCREEN_SIZE[1]/2)
+        screen.blit(text,textRect)
 
     pygame.display.flip()
 pygame.quit()
